fb87bd54bd200b593ff49518d7ce50d2a6cbca74
nawaf alwisidi <nawafalwisidi@gmail.com>
2022-08-01 06:38:01 +0300

    .

--- brands_products/urls.py
+++ brands_products/urls.py
@@ -5,14 +5,14 @@ from . import views
 app_name = "brands_products"
 
 urlpatterns = [
-    path("brands/create/", views.create_brand, name="create_brand"),
-    path("brands/read/", views.read_brands, name="read_brands"),
+    path("brands/create/", views.add_brand, name="add_brand"),
+    path("brands/read/", views.list_brands, name="list_brands"),
     path("brands/update/<brand_id>/", views.update_brand, name="update_brand"),
     path("brands/delete/<brand_id>/", views.delete_brand, name="delete_brand"),
 
-    path("create/", views.create_product, name="add_product"),
-    path('brand_products/<brand_id>/', views.read_brand_products, name="list_brand_products"),
-    path("read/", views.read_products, name="list_products"),
+    path("create/", views.add_product, name="add_product"),
+    path('brand_products/<brand_id>/', views.list_brand_products, name="list_brand_products"),
+    path("read/", views.list_products, name="list_products"),
     path("update/<product_id>/", views.update_product, name="update_product"),
     path("delete/<product_id>/", views.delete_product, name="delete_product"),
 ]
