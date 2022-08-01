fc77ff6118efbb499c7add98cb05c2427bf8f1ad
saud <s.s.alghamdi@hotmail.com>
2022-08-01 14:45:29 +0300

    remove some comments

--- day_6/eCommerce/views.py
+++ day_6/eCommerce/views.py
@@ -117,29 +117,3 @@ def brand_products_list(request: Request, brand_title):
         return Response(data, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"msg" : f"The brand with ID No:{brand_title} is not Found!"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-# @api_view(["GET"])
-# def list_books(request : Request):
-#     print(Book.objects.count()) #returns the total count of books in the database, for testing only
-#     books_with_high_rating_count = Book.objects.filter(rating__gte=4).count() #get count of books greater thank 4
-#     print(books_with_high_rating_count) #for testing only
-
-#     skip = int(request.query_params.get("skip", 0))
-#     get = int(request.query_params.get("get", 10))
-
-#     if "search" in request.query_params:
-#         search_phrase = request.query_params["search"]
-#         all_books = Book.objects.filter(title__startswith=search_phrase)[skip:get]
-#     else:
-#         all_books = Book.objects.all().order_by('-rating')[skip:get]
-
-#     all_books_list = [{"id" : book.id, "title" : book.title, "rating" : book.rating, "pub_date" : book.pub_date} for book in all_books]
-
-#     res_data = {
-#         "msg" : "A list of All Books",
-#         "books" : all_books_list
-#     }
-
-#     return Response(res_data, status=status.HTTP_200_OK)
