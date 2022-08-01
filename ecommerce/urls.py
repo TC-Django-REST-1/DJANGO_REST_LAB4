652baa438e954830c20bce75deadec20906087bc
Abdulrahman Alazwari <aazwari@gmail.com>
2022-08-01 08:53:17 +0300

    ecommerce app, put, delete methods done and tested successfully

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_brand, add_product, list_brand, list_product
+from .views import add_brand, add_product, list_brand, list_product, update_brand, update_product, delete_brand, deleted_product
 app_name = 'ecommerce'
 
 urlpatterns = [
@@ -9,5 +9,11 @@ urlpatterns = [
     # get
     path('brand/', list_brand, name='list_brand'),
     path('product/', list_product, name='list_product'),
+    # put
+    path('brand/update/<id>/', update_brand, name='update_brand'),
+    path('product/update/<id>/', update_product, name='update_product'),
+    # delete
+    path('brand/delete/<id>/', delete_brand, name='delete_brand'),
+    path('product/delete/<id>/', deleted_product, name='delete_product'),
 ]
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Brand, Product
 from .serializers import ProductSerilaizer
 from rest_framework import status
@@ -33,6 +33,14 @@ def add_product(request: Request):
 @api_view(["GET"])
 def list_brand(request: Request):
     brands = Brand.objects.all()
+
+    skip = int(request.query_params.get('skip', 0))
+    get = int(request.query_params.get('get', 10))
+
+    if 'search' in request.query_params:
+        search = request.query_params['search']
+        brands = Brand.objects.filter(title__startswith=search)[skip:get]
+
     brands = [
         {'id': brand.id, 'title': brand.title, 'description': brand.description, 'established_at': brand.established_at, 'city': brand.city}
         for brand in brands]
@@ -43,3 +51,49 @@ def list_product(request: Request):
     products = Product.objects.all()
     products = ProductSerilaizer(instance=products, many=True).data
     return Response({'products': products})
+
+
+# put method
+@api_view(['PUT'])
+def update_brand(request: Request, id):
+    title = request.data['title']
+    description = request.data['description']
+    established_at = request.data['established_at']
+    city = request.data['city']
+
+    brand = get_object_or_404(Brand, id=id)
+    brand.title = title
+    brand.description = description
+    brand.established_at = established_at
+    brand.city = city
+    brand.save()
+
+    return Response({'msg': 'Brand updated successfully'})
+
+
+@api_view(['PUT'])
+def update_product(request: Request, id):
+    product = get_object_or_404(Product, id=id)
+    product = ProductSerilaizer(instance=product, data=request.data)
+
+    if product.is_valid():
+        product.save()
+    else:
+        return Response({'msg': 'Could not update', 'errors': product.errors}, status=status.HTTP_403_FORBIDDEN)
+    
+    return Response({'msg': 'Product updated successfully'})
+
+
+# delete method
+@api_view(['DELETE'])
+def delete_brand(request: Request, id):
+    brand = get_object_or_404(Brand, id=id)
+    brand.delete()
+    return Response({'msg': 'Brand deleted successfully'})
+
+
+@api_view(['DELETE'])
+def deleted_product(request: Request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    return Response({'msg': 'Product deleted successfully'})
