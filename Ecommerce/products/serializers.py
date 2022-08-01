cbab83aa343b223396f025530e14b88cb5af402d
EngReem <itsjustrimo@gmail.com>
2022-08-01 12:49:46 +0300

    first commit

---
+++ Ecommerce/products/serializers.py
@@ -0,0 +1,18 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+
+from .models import Brand, Product
+
+
+class BrandSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
